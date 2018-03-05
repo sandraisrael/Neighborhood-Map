6e339beaf3cbdc5c4d0c69063e0ae44ef2c03387
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2018-03-05 21:40:34 +0100

    add data of places

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// MODEL FOR DATA OF PLACES IN NEIGHBORHOOD
+
+var places = [
+    {
+        title: 'Dubai Mall',
+        location: {
+            lat: 25.1985,
+            lng: 55.2796
+        }
+    },
+    {
+        title: 'Burj Khalifa',
+        location: {
+            lat: 25.197525,
+            lng: 55.274288
+        }
+    },
+    {
+        title: 'Burj Al Arab',
+        location: {
+            lat: 25.141050,
+            lng: 55.185978
+        }
+    },
+    {
+        title: 'Atlantis, The Palm',
+        location: {
+            lat: 25.1252128325,
+            lng: 55.1169911987
+        }
+    },
+    {
+        title: 'Palm Jumieriah',
+        location: {
+            lat: 25.1087995648,
+            lng: 55.1360111226
+        }
+    }
+];
