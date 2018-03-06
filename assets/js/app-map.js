8f1e64b4b1be41120c20b1b40802727f6b8ab01f
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2018-03-06 07:03:23 +0100

    show description in infowindow

--- assets/js/app-map.js
+++ assets/js/app-map.js
@@ -28,6 +28,7 @@ function initMap() {
     });
     // Push the marker to our array of markers.
     markers.push(marker);
+    console.log(marker.title)
 
     // Create an onclick event to open an infowindow for each marker.
     marker.addListener('click', function () {
@@ -41,15 +42,36 @@ function initMap() {
 
 // Show info window for all markers
 function populateInfoWindow(marker, infowindow) {
+  var apiresult
   // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div> <h1>' + marker.title + '</h1> </div>');
-    infowindow.open(map, marker);
-    infowindow.addListener('closeclick', function () {
-      infowindow.setMarker = null;
-    });
-  }
+  $.ajax({
+    url: 'https://en.wikipedia.org/w/api.php',
+    data: {
+      format: "json",
+      action: "query",
+      list: "search",
+      srsearch: marker.title,
+      formatversion: 2
+    }, dataType: 'jsonp',
+    headers: {
+      'Api-User-Agent': 'MyCoolTool/1.1 (http://example.com/MyCoolTool/; MyCoolTool@example.com) BasedOnSuperLib/1.4'
+    },
+  }).done(function (data) {
+    console.log(data.query.search["0"].snippet);
+    apiresult = data.query.search["0"].snippet;
+  }).fail(function (err) {
+      alert("Failed to Load data from wikipedia api")
+    }).then(function () {
+
+      if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div> <h1>' + marker.title + '</h1>' + apiresult + '</div>');
+        infowindow.open(map, marker);
+        infowindow.addListener('closeclick', function () {
+          infowindow.setMarker = null;
+        });
+      }
+    })
 
 }
 
